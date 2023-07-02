c7fcaf7a4ffe7dcfae75e981ed4dcd989531c4b2
sh2002vk <sh2002vk@gmail.com>
2023-07-01 23:20:34 -0400

    small updates

--- testing/NLPengine.py
+++ testing/NLPengine.py
@@ -1,4 +1,6 @@
 from Helpers.AWS import AWS
+from config import models
+import spacy
 
 
 class NLPengine:
@@ -9,20 +11,31 @@ class NLPengine:
 
         self.awsClient = AWS()
         self.transcriptPath = transcriptPath
-        self.transcript = {}
-        # self.models =
+        self.user_transcript = self.agent_transcript = None
+        self.models = models
         return
 
     def downloadText(self):
-        """ Pull from s3 """
-        self.transcript = self.awsClient.downloadS3(self.transcriptPath)
+        transcript = self.awsClient.downloadS3(self.transcriptPath)
+        self.parse(transcript)
+
+    def parse(self, transcript):
+        """ Seperates the User and Agent conversation
+
+        :param transcript: input transcript to parse (JSON)
+        :return:
+        """
 
     def listModels(self):
         # RETURN ACTIVE MODELS
-        return
+        return self.models
 
-    def findEntities(self, model):
-        # FIND ENTITIES IN TEXT
-        return
+    def findEntities(self):
+        # TODO: Abstract this so that it can be generalized
+        outputs = []
+        for provider in self.models:
+            for type, model in self.models[provider].items():
+                print(f'Working with {provider} model {model} focused on {type}')
+                provider.load(model)
 
-    # def findEntitiesAggregate(self):
+        return
---
+++ testing/config.py
@@ -0,0 +1,9 @@
+import spacy
+
+# TODO: Add stanfordNER and BERT for confirmational accuracy
+models = {
+    spacy: {
+        "accuracy": "en_core_web_trf",
+        "speed": "en_core_web_sm"
+    }
+}
